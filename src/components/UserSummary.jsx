4e66124f0cdab4321a41fddc0d664f794925adc1
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-07-10 11:30:34 -0400

    almost there

--- src/components/UserSummary.jsx
+++ src/components/UserSummary.jsx
@@ -1,12 +1,27 @@
-
+import { useState } from "react"
 
 function UserSummary(props) {
 
+const [showMore, setShowMore] = useState(false)
+function flipFalse() {
+    setShowMore(prevBoo => !prevBoo)
+}
 
     return (
 
         <div>
-        {props.userData == null ? ("There is no user data") : (JSON.stringify(props.userData))}
+        {props.userData == null ? ("There is no user data") : (props.userData.data.results.map((data,key) => {
+            return (
+                
+                <div key={key}>
+                  <h2>   {data.name.title} {data.name.first} {data.name.last} </h2>
+                  <h2>{data.email}</h2>
+                  
+                  <button onClick={flipFalse}>Show/Hide</button>
+                  {console.log(showMore)}
+                </div>
+              )
+        }))}
         
         </div>
     )
